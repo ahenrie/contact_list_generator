8065a41f12d674f51248f69d16236fbb94c49466
A. Henrie <103060170+ahenrie@users.noreply.github.com>
2023-10-11 22:11:48 +0000

    Update names.py

--- names.py
+++ names.py
@@ -9,18 +9,19 @@ def get_random_last():
     for col in file:
         last_names.append(col['Last Names'])
 
-    print(last_names)
+    return random.choice(last_names)
 
 def get_random_first():
     male_list = []
     female_list =[]
+    i = random.randint(1, 100)
 
     filename = open('pop_name_usa.csv', 'r')
     file = csv.DictReader(filename)
     for col in file:
         male_list.append(col['Male'])
         female_list.append(col['Female'])
-    
-    return random.choice(male_list), random.choice(female_list)
-
-get_random_first()
+    if i % 2 == 0:
+        return random.choice(male_list)
+    else:
+        return random.choice(female_list)
